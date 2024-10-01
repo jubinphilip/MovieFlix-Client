866cd795658c5bb680873024d457f578449ed51d
Jubin <jubinphilip25@gmail.com>
2024-10-01 11:15:50 +0530

    build commit

--- src/app/user/utils/localstorage.ts
+++ src/app/user/utils/localstorage.ts
@@ -1,22 +1,25 @@
 'use client'
-export const saveState=(state:any)=>{
-    try{
-        const serializedState=JSON.stringify(state);
-        localStorage.setItem('reduxState',serializedState)
+
+export const saveState = (state: any) => {
+    try {
+        if (typeof window !== 'undefined') { // Check if running in the browser
+            const serializedState = JSON.stringify(state);
+            localStorage.setItem('reduxState', serializedState);
+        }
+    } catch (error) {
+        console.log(error);
     }
-    catch(error){
-        console.log(error)
+};
+
+export const loadState = () => {
+    try {
+        if (typeof window !== 'undefined') { // Check if running in the browser
+            const serializedState = localStorage.getItem('reduxState');
+            if (serializedState === null) return undefined;
+            return JSON.parse(serializedState);
+        }
+    } catch (error) {
+        console.log("Error", error);
+        return undefined;
     }
-}//the items from redux is stored at localstorage and whenever a page reload occur redux calls this function and the data is stored to redux from localstorage
-export const loadState=()=>{
-    try{
-        const serializedState=localStorage.getItem('reduxState');
-        if(serializedState===null) 
-            return undefined;
-        return JSON.parse(serializedState);
-    }catch(error)
-    {
-        console.log("Error",error)
-        return undefined
-    }
-}
+};
